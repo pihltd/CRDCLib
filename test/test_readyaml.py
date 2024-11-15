ac4f37be600c27376031124fdf17cd0ca15e313d
Todd Pihl <todd.pihl@gmail.com>
2024-11-15 11:16:42 -0500

    Various testing changes

--- test/test_readyaml.py
+++ test/test_readyaml.py
@@ -1,10 +1,13 @@
 import unittest
 from CRDCLib import readYAML
+from pathlib import Path
 
 class TestReadYaml(unittest.TestCase):
     def test_readyaml(self):
+        TESTPATH=Path(__file__).parent
         answer = {'first': ['second', 'third', 'fourth'], 'fifth': {'sixth': 'seventh'}}
-        yamltestfile = r"yamltestfile.yml"
+        #yamltestfile = r"yamltestfile.yml"
+        yamltestfile = TESTPATH / 'yamltestfile.yml'
         self.assertEqual(readYAML(yamltestfile), answer)
     
 if __name__ == "__main__":
